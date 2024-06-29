954cc6076269d8947004166fd9648ad132dd480d
Hosting-Api <131949339+Hosting-Api@users.noreply.github.com>
2024-06-29 14:10:32 +0530

    Update page.jsx

--- src/app/admin/edit/page.jsx
+++ src/app/admin/edit/page.jsx
@@ -1,44 +1,50 @@
-// import BugCard from "@/BugCard";
-import BugCard from "@/app/components/BugCard";
+// src/app/edit/page.js
 import React from "react";
-
+import BugCard from "../components/BugCard";
+// import dynamic from "next/dynamic";
+// const BugCardU = dynamic(() => import('../components/BugCardU'), {
+//   ssr: false,
+// })
 const getBugs = async () => {
   try {
-    const res = await fetch(`http://bug-portal.vercel.app/api/bugs/`, {
+    const res = await fetch(`https://bug-portal.vercel.app/api/bugs/`, {
       method: "GET",
       cache: "no-cache",
     });
-    return res.json();
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return await res.json();
   } catch (error) {
-    console.log("Failed to get Bugs ", error);
+    console.log("Failed to get Bugs", error);
+    return []; // Return an empty array or some fallback value
   }
 };
 
 export default async function Page() {
-  const bugs  = await getBugs();
+  const bugs = await getBugs();
 
   const uniqueCategories = [...new Set(bugs.map(({ category }) => category))];
 
   return (
     <>
-      <h3 className="text-center"> List Of Bugs displayed Here </h3>
+      <h3 className="text-center">List Of Bugs displayed Here</h3>
       <div className="p-5">
         <div>
-          {bugs &&
+          {bugs.length > 0 &&
             uniqueCategories.map((uniqueCategory, categoryIndex) => (
               <div key={categoryIndex} className="mb-4">
                 <h2>{uniqueCategory}</h2>
                 <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-            {bugs
-              .filter((bug) => bug.category === uniqueCategory)
-              .map((filteredBug, _index) => (
-                <BugCard id={_index} key={_index} bug={filteredBug} />
-              ))}
-          </div>
-        </div>
+                  {bugs
+                    .filter((bug) => bug.category === uniqueCategory)
+                    .map((filteredBug, _index) => (
+                      <BugCard id={_index} key={_index} bug={filteredBug} />
+                    ))}
+                </div>
+              </div>
             ))}
-          
-          </div>
+        </div>
       </div>
     </>
   );
